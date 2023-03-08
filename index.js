4493473920e1c0e4e93a48f2a193aa3356fc0df6
Sikidd <114604191+Sikidd@users.noreply.github.com>
2023-03-08 04:45:39 +0000

    added pages and content

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+//here's a json file of user data
+var data = require('./data/test.json');
+
+app.get('/', (req, res) => {
+  var title = "Home Page";
+  res.render('pages/index', { 'title': title });
+});
+
+app.get('/about', (req, res) => {
+  var title = "About Page";
+    res.render('pages/about',{'title':title});
+});
+
+app.get('/sports', (req, res) => {
+  var title = "Sports Page";
+    res.render('pages/sports',{'title':title});
+});
+
+app.get('/contact', (req, res) => {
+  var title = "Contact Page";
+    res.render('pages/contact',{'title':title});
+});
+
+    //users index is our list page
+
+
+
+    //add user/view route - we are cheating by using the array index - 1
+
+
+app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    console.log(data);
+});
